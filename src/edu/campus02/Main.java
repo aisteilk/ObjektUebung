e5bb66aa9302523d458454635deebe90d990b656
aisteilk <aistemariailk@gmail.com>
2020-03-13 17:05:04 +0100

    Initial commit

---
+++ src/edu/campus02/Main.java
@@ -0,0 +1,52 @@
+package edu.campus02;
+
+public class Main {
+
+    public static void main(String[] args) {
+        haus();
+
+    }
+
+    public static void haus () {
+
+        boolean fenster10 = true;
+
+        if (fenster10) {
+            System.out.println("Das Haus gehört dem Hans!");
+        } else {
+            System.out.println("Wem gehört das Haus?");
+        }
+
+       boolean zimmer8 = true;
+
+       if (zimmer8) {
+           System.out.println("Das Haus gehört dem Hans!");
+       } else {
+           System.out.println("Wem gehört das Haus?");
+       }
+
+        boolean blau = true;
+
+        if (blau) {
+            System.out.println("Das Haus gehört dem Hans!");
+        } else {
+            System.out.println("Wem gehört das Haus?");
+        }
+
+         boolean jahr1990 = true;
+
+         if (jahr1990) {
+             System.out.println("Das Haus gehört dem Hans!");
+         } else {
+             System.out.println("Wem gehört das Haus?");
+         }
+
+        boolean hansWurst = true;
+
+        if (hansWurst) {
+            System.out.println("Das Haus gehört dem Hans!");
+        } else {
+            System.out.println("Wem gehört das Haus");
+        }
+    }
+}
